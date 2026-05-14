ac5bc85d7b024c9f35225c56a024b67d228db798
oe16-png <oe16@humboldt.edu>
2026-05-14 13:04:10 -0700

    uploading project
    hopefully this is the right project

---
+++ javascrpit stuffs.js
@@ -0,0 +1,34 @@
+function yap() {
+sentences = document.getElementById("text").value.split(". ")
+currentIndex = 0
+readnext()
+}
+let sentences = []
+let currentIndex= 0
+
+function readnext() {
+if (currentIndex>sentences.length) return
+let utterance= new SpeechSynthesisUtterance(sentences[currentIndex])
+let levelofyap = parseFloat (document.getElementById("speedbox").value)
+utterance.rate = levelofyap
+speechSynthesis.speak(utterance)
+}
+
+function wait() {
+    ispaused = true
+    speechSynthesis.pause()
+}
+
+function go() {
+    ispaused = false
+    speechSynthesis.resume()
+}
+
+rate.onchange = () => {
+  rateValue.textContent = rate.value;
+}
+/*
+
+*/
+utterance.rate
+speedbox
