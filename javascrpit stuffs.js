8916660b0884fff35b15918952a7eca8e9e2dbed
oe16-png <oe16@humboldt.edu>
2026-05-22 17:23:20 -0700

    backup my files cause my computer gave up on me

--- javascrpit stuffs.js
+++ javascrpit stuffs.js
@@ -6,12 +6,14 @@ readnext()
 let sentences = []
 let currentIndex= 0
 let rate = 1
-
+let pitch = 1
 function readnext() {
 if (currentIndex>sentences.length) return
 let utterance= new SpeechSynthesisUtterance(sentences[currentIndex])
 let levelofyap = parseFloat (document.getElementById("speedbox").value)
 utterance.rate = levelofyap
+let squeaking = parseFloat (document.getElementById("squeeker").value)
+utterance.pitch = squeaking
 speechSynthesis.speak(utterance)
 }
 
@@ -28,12 +30,28 @@ function go() {
 rate.onchange = () => {
   rateValue.textContent = rate.value;
 }
+pitch.onchange = () => {
+  rateValue.textContent = pitch.value;
+}
 
 function end(){
     speechSynthesis.cancel()
 }
-/*
 
-*/
+let $input = document.querySelector("#speedbox") ;
+const SPEEDBOX_ALLOWED_CHARS_REGEXP = /[0-9.]+/
+$input.addEventListener("beforeinput", e =>
+    {if (e.data && !SPEEDBOX_ALLOWED_CHARS_REGEXP.test(e.data)){e.preventDefault();
+
+}
+}) ;
+
+
+
+
+
+/*
 utterance.rate
 speedbox
+
+*/
